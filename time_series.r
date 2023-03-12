0013d5a25314cd7d9064d1561e889877bda6f8a2
Juan Duenas <jddg1000@gmail.com>
2023-03-11 23:52:05 -0500

    Iniciamos creando nuestra primer trabajo en R para series temporales, especialmente AR, MA y ARMA

---
+++ time_series.r
@@ -0,0 +1,19 @@
+#Empezamos limpiando el entorno
+remove(list = ls())
+
+#paquetes
+
+library(forecast)    # Para hacer pronósticos con modelos arima
+library(lmtest)      # Significancia individual de los coeficientes ARIMA
+library(urca)        # Prueba de raíz unitaria
+library(tseries)     # Para estimar modelos de series de tiempo y hacer pruebas de supuestos.
+library(readxl)      # Para leer archivos de excel
+library(stargazer)   # Para presentar resultados más estéticos.
+library(psych)       # Para hacer estadísticas descriptivas
+library(seasonal)    # Para desestacionalizar series
+library(aTSA)        # Para hacer la prueba de efectos ARCH
+library(astsa)       # Para estimar, validar y hacer pronósticos para modelos ARIMA/SARIMA  
+library(xts)         # Para utilizar objetos xts 
+library(tidyverse)   # Conjunto de paquetes (incluye dplyr y ggplot2)
+library(readxl)      # Para leer archivos excel 
+library(car)         # Para usar la función qqPlot
