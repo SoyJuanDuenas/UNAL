12a63023b578248d9cc7277ca9cb6dbe5a957295
Juan Duenas <jddg1000@gmail.com>
2023-03-14 09:00:16 -0500

    (add) new changes to this file, documentation and plots

--- Econometría_Espacial/codigos_graficas_EE.r
+++ Econometría_Espacial/codigos_graficas_EE.r
@@ -22,7 +22,7 @@ library(dplyr)
 library(maptools)
 library(tmaptools)
 
-setwd("C:/Users/Rama IEEE UN/Downloads/Códigos")
+setwd("C:/Users/PC/Downloads/Códigos/Códigos")
 source("northarrow.R")
 source("scalebar.R")
 source("moranbi.test.R")
@@ -38,7 +38,7 @@ source("spcorrelogram.bi.R")
 source("moranbi1.test.R")
 source("moran.bi1.R")
 
-setwd("C:/Users/Rama IEEE UN/Downloads/UPZ_EM2017")
+setwd("C:/Users/PC/Downloads/UPZ_EM2017")
 UPZ_Bog <- readOGR(dsn="ShapeTesis_Ocupa.shp")
 
 #Datos importantes matriz tipo Rook
@@ -53,6 +53,7 @@ plot(rook.fr,  coordinates(UPZ_Bog),  add=TRUE,  col="red")
 queen.fr <- poly2nb(UPZ_Bog, queen = TRUE)
 summary(queen.fr)
 #Graficar Conexiones tipo Queen
+upz_nbr <- poly2nb(UPZ_Bog, queen=T)
 plot(UPZ_Bog, col="white", border="grey", lwd=1)
 plot(upz_nbr, coordinates(UPZ_Bog), col="red", lwd=1, add=TRUE)
 
@@ -63,9 +64,37 @@ Wq<-nb2listw(upz_nbr, style="W")
 moran.test(UPZ_Bog$SharePetro, listw=Wq, randomisation = FALSE, alternative = "two.sided")
 moran.mc(UPZ_Bog$SharePetro, Wq, nsim=99)
 
-proj4string(UPZ_Bog)
-projInfo(type = "proj")
+#cambiamos el sistema de coordenadas
+
 UPZ_Bog_Geo <- spTransform(UPZ_Bog, "+proj=longlat")
-UPZ_Bog_Geo@bbox
 coordsgeo <- coordinates(UPZ_Bog_Geo)
 
+#Ahora creamos una matriz de vecinos basada en distancia, con esto podemos colocar en un vector cuantos vecinos tiene cada punto
+
+k.poly <- knn2nb(knearneigh(coordsgeo, longlat = TRUE))
+critical.threshold.poly <- max(unlist(nbdists(k.poly,coordsgeo, longlat = TRUE)))
+nb.dist.band.poly <- dnearneigh(coordsgeo, 0, critical.threshold.poly, longlat = TRUE)
+
+#Graficamos un histograma con el numero de vecinos por cada vecindario
+
+poly.nb.card <- card(nb.dist.band.poly)
+ggplot() +
+  geom_histogram(aes(x=poly.nb.card)) +
+  xlab("Number of Neighbors")
+
+#Graficamos el mapa de Bogotá y sobre este las conexiones entre los vecinos de la matriz basada en distancia
+
+plot(UPZ_Bog_Geo,col="gray", border="blue", lwd=1)
+plot(nb.dist.band.poly, coordsgeo, add=T, lwd=.2, col="red", cex = .5)
+
+# Uso matriz con banda de distancia para estimación del índice de moran via normalidad y via monte carlo
+
+wkd <- nb2listw(neighbours=nb.dist.band.poly, style="W")
+moran.test(UPZ_Bog$SharePetro, listw=wkd, randomisation = FALSE, alternative = "two.sided")
+moran.mc(UPZ_Bog$SharePetro, wkd, nsim=99)
+
+#podemos hacer este proceso tambien con un numero de k vecinos especificado
+
+k6 <- knn2nb(knearneigh(coordsgeo, k = 6))
+plot(UPZ_Bog_Geo,col="gray", border="blue", lwd=1)
+plot(k6, coordsgeo, add=T, lwd=.2, col="red", cex = .5)
