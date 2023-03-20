40eb25d1397fd4d7e4a02c0b34b27838588f147c
Juan Duenas <jddg1000@gmail.com>
2023-03-19 22:31:55 -0500

    (new) initial commit taller_1 for econometrics II

---
+++ Econometría_II/taller_1.r
@@ -0,0 +1,10 @@
+##____________________________
+##____________________________
+#       UNIVERSIDAD NACIONAL DE COLOMBIA - FACULTAD DE CIENCIAS ECONÓMICAS
+#                         ECONOMETRÍA II - 2023-I
+#                             Trabajo 1, Marzo 
+##____________________________
+##____________________________
+
+# Para la realización del trabajo se uilizará el Índice de producción real de la 
+# industria manufacturera colombiana
