6808e78b6abb201af428c8d34c0082872669a400
Juan David Dueñas <jddg1000@gmail.com>
2023-08-07 19:57:27 -0500

    create dinamic modelation dir and first homework initial commit

--- Macro_III/Trabajo_Final/codigo.R
+++ Macro_III/Trabajo_Final/codigo.R
@@ -1,5 +1,7 @@
 ## TRABAJO macro III
 
+# Codigo para limpiar el Environment
+remove(list = ls())
 ##Importamos librerias
 
 library(readxl)
@@ -10,34 +12,30 @@ library(tseries)
 paises_de_interes <- c("Colombia", "Francia", "Kenya", "Sudáfrica")
 variables_de_interes <- c("pib", "pib_per_capita", "pt", "fbcf", "fbcf_per_capita", "pea")
 
-datasets <- list(
-  pib = read_excel("C:/Users/Rama IEEE UN/Downloads/PIB(UMN precios constantes).xls", sheet = 1),
-  pib_per_capita = read_excel("C:/Users/Rama IEEE UN/Downloads/PIB per cápita (UMN a precios constantes).xls", sheet = 1),
-  pt = read_excel("C:/Users/Rama IEEE UN/Downloads/Poblacion total.xls", sheet = 1),
-  fbcf = read_excel("C:/Users/Rama IEEE UN/Downloads/Formación bruta de capital fijo (UMN a precios constantes).xls", sheet = 1),
-  fbcf_per_capita = read_excel("C:/Users/Rama IEEE UN/Downloads/Formación bruta de capital fijo (% del PIB).xls", sheet = 1),
-  pea = read_excel("C:/Users/Rama IEEE UN/Downloads/pea.xls", sheet = 1)
-)
 
-# Limitar los datasets a los países de interés
-for (i in seq_along(variables_de_interes)) {
-  datasets[[i]] <- subset(datasets[[i]], `Data Source` %in% paises_de_interes)
+datasets <- list(
+  pib = read.csv("https://raw.githubusercontent.com/SoyJuanDuenas/UNAL/master/Macro_III/Trabajo_Final/PIB.csv", sep = ",", quote = "'", header = F),
+  pib_per_capita = read.csv("https://raw.githubusercontent.com/SoyJuanDuenas/UNAL/master/Macro_III/Trabajo_Final/PIB%20per%20c%C3%A1pita%20(UMN%20a%20precios%20constantes).csv", sep = ",", quote = "'", header = F ),
+  pt = read.csv("https://raw.githubusercontent.com/SoyJuanDuenas/UNAL/master/Macro_III/Trabajo_Final/poblacion%20total.csv", sep = ",", quote = "'", header = F ),
+  fbcf = read.csv("https://raw.githubusercontent.com/SoyJuanDuenas/UNAL/master/Macro_III/Trabajo_Final/FBCF.csv", sep = ",", quote = "'", header = F ),
+  fbcf_porcentaje = read.csv("https://raw.githubusercontent.com/SoyJuanDuenas/UNAL/master/Macro_III/Trabajo_Final/PIB%20per%20c%C3%A1pita%20(UMN%20a%20precios%20constantes).csv", sep = ",", quote = "'", header = F ),
+  pea = read.csv("https://raw.githubusercontent.com/SoyJuanDuenas/UNAL/master/Macro_III/Trabajo_Final/pea.csv", sep = ",", quote = "'", header = F )
+
+# Hacemos un datacleaning apropiado y creamos las TS
+
+                   for (i in seq_along(variables_de_interes)) {
+  datasets[[i]] <- subset(datasets[[i]], `Data.Source` %in% paises_de_interes)
+  datasets[[i]] <- datasets[[i]][, -c(1,2,3,4)]
+  rownames(datasets[[i]])[1] <- "Colombia"
+  rownames(datasets[[i]])[2] <- "Francia"
+  rownames(datasets[[i]])[3] <- "Kenya"
+  rownames(datasets[[i]])[4] <- "Sudafrica"
+  colnames(datasets[[i]]) <- c(1960:2021)
 }
 
 
-#convertimos a TS
-
-ts(data = pib ,start = 1960, frequency = 1)
-ts(data = pib_per_capita ,start = 1960, frequency = 1)
-ts(data = pt ,start = 1960, frequency = 1)
-ts(data = fbcf ,start = 1960, frequency = 1)
-ts(data = fbcf_per_capita ,start = 1960, frequency = 1)
-ts(data = pea ,start = 1960, frequency = 1)
-
-for(i %in% c("Colombia", "Francia", "Kenya", "Sudáfrica")){
-  
-} 
 
 #Dado que no tenemos los datos de la PEA desde 1960 la estimamos a partir de la 
 #PT
+
 
